125122e6cfc44c10e8f1886f3fecef17b7b3d4c1
Darin Minamoto <darinm223@gmail.com>
2015-03-03 13:30:22 -0800

    Fixed repeat category problem

--- src/edu/ucla/cs/cs144/ItemServlet.java
+++ src/edu/ucla/cs/cs144/ItemServlet.java
@@ -44,13 +44,7 @@ public class ItemServlet extends HttpServlet implements Servlet {
         "Notation",
     };
 
-    //hashmaps and arraylist to store
-    static Map<String, Bidder> bidderMap = new HashMap<String, Bidder>();
-    // static Map<String, Seller> sellerMap = new HashMap<String, Seller>();
-    static ArrayList<Bid> bidList = new ArrayList<Bid>();
-    // static ArrayList<Item> itemList = new ArrayList<Item>();
-    // static ArrayList<ItemCategory> itemCategoryList = new ArrayList<ItemCategory>();
-    static ArrayList<String> categoryList = new ArrayList<String>();
+
 
     static class MyErrorHandler implements ErrorHandler {
         
@@ -169,120 +163,126 @@ public class ItemServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+
+        //hashmaps and arraylist to store
+        Map<String, Bidder> bidderMap = new HashMap<String, Bidder>();
+        ArrayList<Bid> bidList = new ArrayList<Bid>();
+        ArrayList<String> categoryList = new ArrayList<String>();
+        
         // your codes here
         String id = request.getParameter("id");
         String xml = AuctionSearchClient.getXMLDataForItemId(id);
 
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            factory.setValidating(false);
-            factory.setIgnoringElementContentWhitespace(true);
-            DocumentBuilder builder;
-            Document doc = null;
-            try {
-                builder = factory.newDocumentBuilder();
-                doc = builder.parse(new InputSource(new StringReader(xml)));
-            } catch (Exception e) {
-                System.out.println(e);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setValidating(false);
+        factory.setIgnoringElementContentWhitespace(true);
+        DocumentBuilder builder;
+        Document doc = null;
+        try {
+            builder = factory.newDocumentBuilder();
+            doc = builder.parse(new InputSource(new StringReader(xml)));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        //check if the item is found
+        if (doc == null) {
+            request.setAttribute("itemFound", false);
+
+            //send to jsp page for display
+            request.getRequestDispatcher("/itemResult.jsp").forward(request, response);
+        }
+        else {
+            Element item = doc.getDocumentElement();
+
+            String itemID = item.getAttribute("ItemID");
+            String name = escapeString(getElementTextByTagNameNR(item, "Name"));
+            
+            //convert currently, buy_price, first_bid to money-string
+            String currently = strip(getElementTextByTagNameNR(item, "Currently"));
+            String buy_price = strip(getElementTextByTagNameNR(item, "Buy_Price"));
+            String first_bid = strip(getElementTextByTagNameNR(item, "First_Bid"));
+
+            String number_of_bids = getElementTextByTagNameNR(item, "Number_of_Bids");
+            Element location_e = getElementByTagNameNR(item, "Location");
+            String location = escapeString(getElementText(location_e));
+            String latitude = location_e.getAttribute("Latitude");
+            String longitude = location_e.getAttribute("Longitude");
+            String country = escapeString(getElementTextByTagNameNR(item, "Country"));
+
+            //convert started and ends
+            String started = convertDateFormat(getElementTextByTagNameNR(item, "Started"));
+            String ends = convertDateFormat(getElementTextByTagNameNR(item, "Ends"));
+
+            //get seller userID and rating
+            Element seller = getElementByTagNameNR(item, "Seller");
+            String s_userID = seller.getAttribute("UserID");
+            String s_rating = seller.getAttribute("Rating");
+
+            //create seller object for request
+            Seller seller_obj = new Seller(s_userID, s_rating);
+
+            //truncate description to 4000 characters if its longer than 4000
+            String description = escapeString(getElementTextByTagNameNR(item, "Description"));
+            if (description.length() > 4000) {
+                description = description.substring(0, 4000);
             }
 
-            //check if the item is found
-            if (doc == null) {
-                request.setAttribute("itemFound", false);
+            //get bids for the item if any
+            Element bids_root = getElementByTagNameNR(item, "Bids");
+            Element[] bids = getElementsByTagNameNR(bids_root, "Bid");
+            for (Element b : bids) {
+                //get bidder info
+                Element bidder = getElementByTagNameNR(b, "Bidder");
+                String b_userID = bidder.getAttribute("UserID");
+                String b_rating = bidder.getAttribute("Rating");
+                String b_location = escapeString(getElementTextByTagNameNR(bidder, "Location"));
+                String b_country = escapeString(getElementTextByTagNameNR(bidder, "Country"));
 
-                //send to jsp page for display
-                request.getRequestDispatcher("/itemResult.jsp").forward(request, response);
+                //check if bidder is already in bidderMap and add bidder if not
+                if (!bidderMap.containsKey(b_userID)) {
+                    Bidder b_bidder = new Bidder(b_userID, b_rating, b_location, b_country);
+                    bidderMap.put(b_userID, b_bidder);
+                }
+
+                //get remaining bid info
+                String time = convertDateFormat(getElementTextByTagNameNR(b, "Time"));
+                String amount = strip(getElementTextByTagNameNR(b, "Amount"));
+
+                //add bid to bidList
+                Bid bid = new Bid(itemID, b_userID, time, amount);
+                bidList.add(bid);
             }
-            else {
-                Element item = doc.getDocumentElement();
 
-                String itemID = item.getAttribute("ItemID");
-                String name = escapeString(getElementTextByTagNameNR(item, "Name"));
-                
-                //convert currently, buy_price, first_bid to money-string
-                String currently = strip(getElementTextByTagNameNR(item, "Currently"));
-                String buy_price = strip(getElementTextByTagNameNR(item, "Buy_Price"));
-                String first_bid = strip(getElementTextByTagNameNR(item, "First_Bid"));
+            //get all categories and put into categories
+            Element[] categories = getElementsByTagNameNR(item, "Category");
+            for (Element c : categories) {
+                //get category and add to categories
+                String category = getElementText(c);
+                categoryList.add(category);
+            }
 
-                String number_of_bids = getElementTextByTagNameNR(item, "Number_of_Bids");
-                Element location_e = getElementByTagNameNR(item, "Location");
-                String location = escapeString(getElementText(location_e));
-                String latitude = location_e.getAttribute("Latitude");
-                String longitude = location_e.getAttribute("Longitude");
-                String country = escapeString(getElementTextByTagNameNR(item, "Country"));
+            //create item object for request
+            Item item_obj = new Item(itemID, name, currently, buy_price, first_bid,
+                            number_of_bids, location, latitude, longitude,
+                            country, started, ends, s_userID, description);
 
-                //convert started and ends
-                String started = convertDateFormat(getElementTextByTagNameNR(item, "Started"));
-                String ends = convertDateFormat(getElementTextByTagNameNR(item, "Ends"));
+            //sort bids by date before passing for display
+            Collections.sort(bidList);
 
-                //get seller userID and rating
-                Element seller = getElementByTagNameNR(item, "Seller");
-                String s_userID = seller.getAttribute("UserID");
-                String s_rating = seller.getAttribute("Rating");
+            //sort categories alphabetically before passing for display
+            Collections.sort(categoryList);
 
-                //create seller object for request
-                Seller seller_obj = new Seller(s_userID, s_rating);
+            //set attributes for the 5 sections
+            request.setAttribute("bidders", bidderMap);
+            request.setAttribute("seller", seller_obj);
+            request.setAttribute("bids", bidList);
+            request.setAttribute("item", item_obj);
+            request.setAttribute("categories", categoryList);
+            request.setAttribute("itemFound", true);
 
-                //truncate description to 4000 characters if its longer than 4000
-                String description = escapeString(getElementTextByTagNameNR(item, "Description"));
-                if (description.length() > 4000) {
-                    description = description.substring(0, 4000);
-                }
-
-                //get bids for the item if any
-                Element bids_root = getElementByTagNameNR(item, "Bids");
-                Element[] bids = getElementsByTagNameNR(bids_root, "Bid");
-                for (Element b : bids) {
-                    //get bidder info
-                    Element bidder = getElementByTagNameNR(b, "Bidder");
-                    String b_userID = bidder.getAttribute("UserID");
-                    String b_rating = bidder.getAttribute("Rating");
-                    String b_location = escapeString(getElementTextByTagNameNR(bidder, "Location"));
-                    String b_country = escapeString(getElementTextByTagNameNR(bidder, "Country"));
-
-                    //check if bidder is already in bidderMap and add bidder if not
-                    if (!bidderMap.containsKey(b_userID)) {
-                        Bidder b_bidder = new Bidder(b_userID, b_rating, b_location, b_country);
-                        bidderMap.put(b_userID, b_bidder);
-                    }
-
-                    //get remaining bid info
-                    String time = convertDateFormat(getElementTextByTagNameNR(b, "Time"));
-                    String amount = strip(getElementTextByTagNameNR(b, "Amount"));
-
-                    //add bid to bidList
-                    Bid bid = new Bid(itemID, b_userID, time, amount);
-                    bidList.add(bid);
-                }
-
-                //get all categories and put into categories
-                Element[] categories = getElementsByTagNameNR(item, "Category");
-                for (Element c : categories) {
-                    //get category and add to categories
-                    String category = getElementText(c);
-                    categoryList.add(category);
-                }
-
-                //create item object for request
-                Item item_obj = new Item(itemID, name, currently, buy_price, first_bid,
-                                number_of_bids, location, latitude, longitude,
-                                country, started, ends, s_userID, description);
-
-                //sort bids by date before passing for display
-                Collections.sort(bidList);
-
-                //sort categories alphabetically before passing for display
-                Collections.sort(categoryList);
-
-                //set attributes for the 5 sections
-                request.setAttribute("bidders", bidderMap);
-                request.setAttribute("seller", seller_obj);
-                request.setAttribute("bids", bidList);
-                request.setAttribute("item", item_obj);
-                request.setAttribute("categories", categoryList);
-                request.setAttribute("itemFound", true);
-
-                //send to jsp page for display
-                request.getRequestDispatcher("/itemResult.jsp").forward(request, response);
+            //send to jsp page for display
+            request.getRequestDispatcher("/itemResult.jsp").forward(request, response);
         }
     }
 }
