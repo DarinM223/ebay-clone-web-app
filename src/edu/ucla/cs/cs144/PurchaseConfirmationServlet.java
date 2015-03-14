8e70a536b7941afeea8fbe1a1f78dc6918fc0b16
Darin Minamoto <darinm223@gmail.com>
2015-03-14 15:14:28 -0700

    Added skeleton purchase servlets

---
+++ src/edu/ucla/cs/cs144/PurchaseConfirmationServlet.java
@@ -0,0 +1,20 @@
+package edu.ucla.cs.cs144;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by D_2 on 3/14/2015.
+ */
+public class PurchaseConfirmationServlet extends HttpServlet implements Servlet {
+    public PurchaseConfirmationServlet() {}
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ src/edu/ucla/cs/cs144/PurchaseServlet.java
@@ -0,0 +1,20 @@
+package edu.ucla.cs.cs144;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by D_2 on 3/14/2015.
+ */
+public class PurchaseServlet extends HttpServlet implements Servlet {
+    public PurchaseServlet() {}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
